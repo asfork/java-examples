f370fca5a8dfb6ef45c425791641b15997068ac8
Steve Zhang <stevzhg@gmail.com>
2020-08-10 15:58:25 +0800

    add VolatileExample

---
+++ src/concurrent/VolatileExample.java
@@ -0,0 +1,29 @@
+package concurrent;
+
+public class VolatileExample {
+    public static volatile int found = 0;
+
+    public static void change() {
+        found = 1;
+    }
+
+    public static void main(String[] args) {
+        new Thread((Runnable) () -> {
+            System.out.println("Wait for target");
+            while (0 == found) {
+            }
+            System.out.println("Target arrived");
+        }, "Main thread").start();
+
+        new Thread((Runnable) () -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Send signal to main");
+
+            change();
+        }, "Target Thread").start();
+    }
+}
